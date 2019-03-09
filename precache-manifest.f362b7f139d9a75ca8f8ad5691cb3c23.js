bba692cd5946d1f791b45d35caf0e89c9d21ca90
assalielmehdi@gmail.com <assalielmehdi@gmail.com>
2019-03-09 10:45:09 +0100

    Updates

---
+++ precache-manifest.f362b7f139d9a75ca8f8ad5691cb3c23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca6696e082dacb40ea24e7e237cff257",
+    "url": "/portfolio/static/media/icon.ca6696e0.png"
+  },
+  {
+    "revision": "da10a58fe9e9cc6f73744a8ebd6ecc6c",
+    "url": "/portfolio/static/media/dots.da10a58f.svg"
+  },
+  {
+    "revision": "088a7d1a0662facb5875cf23fed62789",
+    "url": "/portfolio/static/media/cover-iceberg.088a7d1a.jpg"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "ca267868455baa473c5e",
+    "url": "/portfolio/static/js/main.ca267868.chunk.js"
+  },
+  {
+    "revision": "12f1f0dbb7c616486dfb",
+    "url": "/portfolio/static/js/2.12f1f0db.chunk.js"
+  },
+  {
+    "revision": "ca267868455baa473c5e",
+    "url": "/portfolio/static/css/main.f2360736.chunk.css"
+  },
+  {
+    "revision": "9ade84218c98dedc1e9e39d09fbaa625",
+    "url": "/portfolio/index.html"
+  }
+];
