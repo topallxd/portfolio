cb844fbc245e9418c44bfc7f912e8491fb5ae4ef
assalielmehdi@gmail.com <assalielmehdi@gmail.com>
2019-03-10 20:48:18 +0100

    Updates

---
+++ precache-manifest.63f79cdd6884037884e1344b4f6cf7ab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef53e5e8bb9a429b9404bd2eafe5d893",
+    "url": "/portfolio/static/media/spring-boot-logo.ef53e5e8.svg"
+  },
+  {
+    "revision": "bf4644890854b86740e8",
+    "url": "/portfolio/static/css/main.9e473aeb.chunk.css"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "63fdd800f63fe76b7cc0",
+    "url": "/portfolio/static/js/2.63fdd800.chunk.js"
+  },
+  {
+    "revision": "ca6696e082dacb40ea24e7e237cff257",
+    "url": "/portfolio/static/media/icon.ca6696e0.png"
+  },
+  {
+    "revision": "bf4644890854b86740e8",
+    "url": "/portfolio/static/js/main.bf464489.chunk.js"
+  },
+  {
+    "revision": "3a3f271a4b8e6ec75a4a58654fd7d782",
+    "url": "/portfolio/static/media/adobe-xd-logo.3a3f271a.png"
+  },
+  {
+    "revision": "7e3444c72ab7764fcd26db7d1f31b7ad",
+    "url": "/portfolio/static/media/compiler-logo.7e3444c7.png"
+  },
+  {
+    "revision": "3de2f9b6b57503cb4335670fdb7a75f9",
+    "url": "/portfolio/static/media/spinner.3de2f9b6.svg"
+  },
+  {
+    "revision": "da10a58fe9e9cc6f73744a8ebd6ecc6c",
+    "url": "/portfolio/static/media/dots.da10a58f.svg"
+  },
+  {
+    "revision": "7b26bce70ef883e9cdf068d50109bcc0",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8ed6f4455d56f6d011e04b4ed6bc6fe8.js"
+  "/portfolio/precache-manifest.63f79cdd6884037884e1344b4f6cf7ab.js"
 );
 
 workbox.clientsClaim();
